93a4a24f48f746a90d5b5b36c1a31a87b81760c4
md-ataur <mdataur15@gmail.com>
2021-12-28 23:45:48 +0600

    file updated

--- index.js
+++ index.js
@@ -7,23 +7,21 @@ const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-// middleware functions
+// Middleware functions
 app.use(cors());
 app.use(express.json());
-
-
 
 // Database Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.juclx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 async function run() {
     try {
         await client.connect();
         const database = client.db("watch_store");
         const productsCollection = database.collection("products");
+        const usersCollection = database.collection("users");
+        const ordersCollection = database.collection("orders");
 
         // POST API to add product
         app.post('/products', async (req, res) => {
@@ -55,6 +53,84 @@ async function run() {
             res.json(result);
         });
 
+        // POST API to add user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        // PUT API to update user
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // PUT API to update user role
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        // GET Api to check role
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        // POST API to add order
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        });
+
+        // GET API to get all orders
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // PUT API to update order status
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateStatus = req.body.status;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = { $set: { status: updateStatus } };
+            const result = await ordersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        // POST API to get orders by email
+        app.post('/orders/byemail', async (req, res) => {
+            const email = req.body;
+            const filter = { email: { $in: email } };
+            const products = await ordersCollection.find(filter).toArray();
+            res.send(products);
+        });
+
+        // DELETE API to delete order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
         // console.log('Successfully database connected');
     }
     finally {
@@ -70,3 +146,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listening at', port);
 });
+
+
+
+/*
+One time:
+1. heroku account open
+2. Heroku software install
+-------------
+Every project
+1. git init
+2. .gitignore (node_module, .env)
+3. push everything to git
+4. make sure you have this script:  "start": "node index.js",
+5. make sure: put process.env.PORT in front of your port number
+6. heroku login
+7. heroku create (only one time for a project)
+8. command: git push heroku main
+-------
+update:
+1. save everything and check locally
+2. git add, git commit-m", git push
+2. git push heroku main
+*/
